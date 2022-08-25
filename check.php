7b3f10cf2304b5b8e44bb9c50c3dd0bf5a6534bc
Piyush <piyush.pandey@innoraft.com>
2022-08-25 16:22:47 +0530

    Remove file

--- check.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['log']=1;
-header("Location: /view/home.php")
-?>
--- view/log.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "You are logged out click on below link to login";
-$link="/view/loginform.php";
-echo "<br>";
-echo "<a href=$link>LOGIN</a>";
-
-?>
